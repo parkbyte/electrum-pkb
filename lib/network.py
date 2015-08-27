cf17482d7718af40620f26847889604722a26d47
ParkByte Limited <support@parkbyte.com>
2015-08-27 22:44:14 +0100

    default port changed

--- lib/network.py
+++ lib/network.py
@@ -14,7 +14,7 @@ from bitcoin import *
 import interface
 from blockchain import Blockchain
 
-DEFAULT_PORTS = {'t':'50001', 's':'50002', 'h':'8081', 'g':'8082'}
+DEFAULT_PORTS = {'t':'5035', 's':'50002', 'h':'8081', 'g':'8082'}
 
 DEFAULT_SERVERS = {
     'electrum-pkb.net': DEFAULT_PORTS,
